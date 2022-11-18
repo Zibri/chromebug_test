d7e46592641c46b4ce28712e0687625d6412ba1c
Zibri <Zibri@users.noreply.github.com>
2022-11-18 18:14:38 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -52,18 +52,7 @@ self.addEventListener("install", event => {
 });
 
 self.addEventListener("activate", event => {
-	// Delete any non-current cache
-	event.waitUntil(
-		caches.keys().then(keys => {
-			Promise.all(
-				keys.map(key => {
-					if (![cacheName].includes(key)) {
-						return caches.delete(key);
-					}
-				})
-			)
-		})
-	);
+	caches.delete(cacheName);
 });
 
 // Offline-first, cache-first strategy
